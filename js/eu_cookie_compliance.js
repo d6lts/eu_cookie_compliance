9a617f920df2acdb1a7c033e51191b7095698dd8
Bela Marai <mibfire@155136.no-reply.drupal.org>
2012-05-10 12:35:16 +0200

    fixed js for IE8

--- js/eu_cookie_compliance.js
+++ js/eu_cookie_compliance.js
@@ -1,9 +1,9 @@
 Drupal.eu_cookie_compliance_popup = function(context) {
-  enabled = Drupal.settings.eu_cookie_compliance.popup_enabled;
+  var enabled = Drupal.settings.eu_cookie_compliance.popup_enabled;
   if(!enabled) {
     return;
   }
-  status = Drupal.eu_cookie_compliance.getCurrentStatus();
+  var status = Drupal.eu_cookie_compliance.getCurrentStatus();
   if (status == 0) {
     $('a').click(function(){
       Drupal.eu_cookie_compliance.changeStatus(1);
@@ -24,19 +24,20 @@ Drupal.eu_cookie_compliance.createPopup = function(html) {
     .height(Drupal.settings.eu_cookie_compliance.popup_height)
     .width(Drupal.settings.eu_cookie_compliance.popup_width)
     .hide();
-
   if(Drupal.settings.eu_cookie_compliance.popup_position) {
+    popup.prependTo("body");
+    var height = popup.height();
     popup.show()
       .attr({"class": "sliding-popup-top"})
-      .css({"top": -1 * Drupal.settings.eu_cookie_compliance.popup_height})
-      .prependTo("body")
+      .css({"top": -1 * height})
       .animate({top: 0}, Drupal.settings.eu_cookie_compliance.popup_delay);
   } else {
+    popup.appendTo("body");
+    height = popup.height();
     popup.show()
       .attr({"class": "sliding-popup-bottom"})
-      .css({"bottom": -1 * Drupal.settings.eu_cookie_compliance.popup_height})
+      .css({"bottom": -1 * height})
       .animate({bottom: 0}, Drupal.settings.eu_cookie_compliance.popup_delay)
-      .appendTo("body");
   }
   Drupal.eu_cookie_compliance.attachEvents();
 }
@@ -54,22 +55,22 @@ Drupal.eu_cookie_compliance.attachEvents = function() {
 }
 
 Drupal.eu_cookie_compliance.getCurrentStatus = function() {
-  search = 'cookie-agreed-'+Drupal.settings.eu_cookie_compliance.popup_language+'=';
-  offset = document.cookie.indexOf(search);
+  var search = 'cookie-agreed-'+Drupal.settings.eu_cookie_compliance.popup_language+'=';
+  var offset = document.cookie.indexOf(search);
   if (offset < 1) {
     return 0;
   }
   offset += search.length;
-  end = document.cookie.indexOf(';', offset);
+  var end = document.cookie.indexOf(';', offset);
   if (end == -1) {
     end = document.cookie.length;
   }
-  value = document.cookie.substring(offset, end);
+  var value = document.cookie.substring(offset, end);
   return parseInt(value);
 }
 
 Drupal.eu_cookie_compliance.changeStatus = function(value) {
-  status = Drupal.eu_cookie_compliance.getCurrentStatus();
+  var status = Drupal.eu_cookie_compliance.getCurrentStatus();
   if (status == value) return;
   if(Drupal.settings.eu_cookie_compliance.popup_position) {
     $(".sliding-popup-top").animate({top: $("#sliding-popup").height() * -1}, Drupal.settings.eu_cookie_compliance.popup_delay, function () {
@@ -98,7 +99,7 @@ Drupal.eu_cookie_compliance.changeStatus = function(value) {
 }
 
 Drupal.eu_cookie_compliance.hasAgreed = function() {
-  status = Drupal.eu_cookie_compliance.getCurrentStatus();
+  var status = Drupal.eu_cookie_compliance.getCurrentStatus();
   if(status == 1 || status == 2) {
     return true;
   }
