bca8d2bbcd3446173656789b067a4bfbfae787c0
borisson_ <borisson_@2393360.no-reply.drupal.org>
2016-08-15 16:48:41 +0530

    Issue #2751799 by borisson_, naveenvalecha: Add test coverage for Configuration Form

--- src/Tests/EuCookieComplianceTest.php
+++ src/Tests/EuCookieComplianceTest.php
@@ -20,23 +20,33 @@ class EuCookieComplianceTest extends EuCookieComplianceTestBasic {
   ];
 
   /**
-   * {@inheritdoc}
-   */
-  protected function setUp() {
-    parent::setUp();
-  }
-
-  /**
    * Test routes.
    */
   public function testRoutes() {
-
     $this->drupalLogin($this->adminUser);
 
     $this->drupalGet('admin/config/system/eu-cookie-compliance');
     $this->assertResponse(200);
 
     $this->assertText('EU Cookie Compliance', 'Right Text');
+  }
+
+  /**
+   * Tests the admin UI.
+   */
+  public function testAdminUI() {
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('admin/config/system/eu-cookie-compliance');
+    $this->assertNoFieldChecked('edit-popup-enabled');
+    $edit = ['popup_enabled' => 1];
+
+    $this->drupalPostForm(NULL, $edit, 'Save configuration');
+    $this->assertFieldChecked('edit-popup-enabled');
+    $this->assertText('Privacy policy link field is required.');
+
+    $edit += ['popup_link' => 'https://drupal.org'];
+    $this->drupalPostForm(NULL, $edit, 'Save configuration');
+    $this->assertText('The configuration options have been saved.');
   }
 
 }
